bdbfc34ddc044cf30c064531a76a2bead615649e
divabsaid <101082272+divabsaid@users.noreply.github.com>
2022-11-27 22:26:02 +0800

    49

--- internal/models/models.go
+++ internal/models/models.go
@@ -137,9 +137,9 @@ func (m *DBModel) InsertOrder(order Order) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `insert into orders (widget_id, transaction_id, status_id, quantity, amount, created_at, updated_at) values (?, ?, ?, ?, ?, ?, ?)`
+	stmt := `insert into orders (widget_id, transaction_id, status_id, quantity, customer_id, amount, created_at, updated_at) values (?, ?, ?, ?, ?, ?, ?, ?)`
 
-	result, err := m.DB.ExecContext(ctx, stmt, order.WidgetID, order.TransactionID, order.StatusID, order.Quantity, order.Amount, time.Now(), time.Now())
+	result, err := m.DB.ExecContext(ctx, stmt, order.WidgetID, order.TransactionID, order.StatusID, order.Quantity, order.CustomerID, order.Amount, time.Now(), time.Now())
 
 	if err != nil {
 		return 0, err
