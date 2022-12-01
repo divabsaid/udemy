52c4209c6f828c2fa0feaa95bf1e60efb8e6be49
divabsaid <101082272+divabsaid@users.noreply.github.com>
2022-12-01 14:40:34 +0800

    79

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -43,9 +43,15 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `insert into tokens (user_id, name, email, token_hash, created_at, updated_at) values (?,?,?,?,?,?)`
+	stmt := `delete from tokens where user_id = ?`
+	_, err := m.DB.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
 
-	_, err := m.DB.ExecContext(ctx, stmt, u.ID, u.LastName, u.Email, t.Hash, time.Now(), time.Now())
+	stmt = `insert into tokens (user_id, name, email, token_hash, created_at, updated_at) values (?,?,?,?,?,?)`
+
+	_, err = m.DB.ExecContext(ctx, stmt, u.ID, u.LastName, u.Email, t.Hash, time.Now(), time.Now())
 	if err != nil {
 		return err
 	}
