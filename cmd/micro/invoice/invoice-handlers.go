8be299e9b8c61bdf73c0c0c1d1975a496cfd84ef
divabsaid <101082272+divabsaid@users.noreply.github.com>
2022-12-07 15:05:06 +0800

    163

--- cmd/micro/invoice/invoice-handlers.go
+++ cmd/micro/invoice/invoice-handlers.go
@@ -3,8 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+type Order struct {
+	ID        int       `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json:"product"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func (app *application) CreateandSendInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, %s", "world")
+	var order Order
+
+	err := app.readJSON(w, r, &order)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	resp.Error = false
+	resp.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
+	app.writeJSON(w, http.StatusCreated, resp)
 }
--- cmd/micro/invoice/invoice-routes.go
+++ cmd/micro/invoice/invoice-routes.go
@@ -18,7 +18,7 @@ func (app *application) routes() http.Handler {
 		MaxAge:           300,
 	}))
 
-	mux.Get("/invoice/create-and-send", app.CreateandSendInvoice)
+	mux.Post("/invoice/create-and-send", app.CreateandSendInvoice)
 
 	return mux
 }
